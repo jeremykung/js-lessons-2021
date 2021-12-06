303c1819251bc295bc25f5dea66ad43debe8bc0f
jeremykung <jeremykung@gmail.com>
2021-12-06 11:25:21 +0800

    updated lesson 2

---
+++ 2-js-logic/script.js
@@ -0,0 +1,41 @@
+/*
+
+0. Expressions and Operators
+++, --, /+-*, !, **
+
+1. If
+2. If else
+3. Else if
+
+4. Assignment vs Comparison
+= vs == vs ===
+< =<
+
+*/
+
+let num = 5
+// console.log(num)
+
+// Increment (Adds 1)
+// Decrement (Subtracts 1)
+num--
+// console.log(num)
+
+// Basic arithmetic (add, subtract, multiply, divide)
+const answer = 5 / 2;
+// console.log(answer)
+
+// = assignment
+const name = "Jackson"
+
+// == comparison (not strict)
+// === comparison (strict)
+
+if (5 > 7) {
+    // if condition is true, do this
+    console.log("hi Nico")
+} else if (5 >= 7) {
+    console.log("your second if check is true")
+} else {
+    console.log("your condition was false")
+}
