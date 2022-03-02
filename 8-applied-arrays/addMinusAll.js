0a771b0d714787b36672a611f88755302c58a44c
Jeremy <jeremykung@gmail.com>
2022-03-02 15:27:44 +0800

    added applied arrays 2 challenges

---
+++ 8-applied-arrays/addMinusAll.js
@@ -0,0 +1,24 @@
+/*
+
+Create a function that adds all the numbers of an array up and returns the sum
+
+1. Your function should accept an array as a method
+2. The return value should be the sum of all array elements
+
+Example:
+An array of [4, 58, 21]
+Return value: 83
+
+Create a second function that subtracts all the elements from a passed value
+
+1. Your function should accept 2 arguments
+    - First argument is an array
+    - Second argument is a value
+2. The return value should be the second argument's value minus all the array elements
+
+Example:
+An array of [2, 8, 5] as first argument
+A value of 100 as second argument
+Return value: 85
+
+*/
---
+++ 8-applied-arrays/biggestDifference.js
@@ -0,0 +1,16 @@
+/*
+
+Create a function that checks an array and finds the biggest difference between numbers
+
+1. Your function should accept an array as a method
+2. It should check for what the lowest number is
+3. Then it should check for what the highest number is
+4. It will then calculate the difference between those numbers and return the difference
+
+Example
+If your array is [12, 5, 2, 18, 15]
+Your return value should be 16
+
+Good luck!
+
+*/
