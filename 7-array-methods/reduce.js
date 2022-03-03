c873ab94df55356563d6bbd267230d6b86dbb8c1
Jeremy <jeremykung@gmail.com>
2022-03-03 11:18:39 +0800

    added nico reduce method

--- 7-array-methods/reduce.js
+++ 7-array-methods/reduce.js
@@ -1,11 +1,39 @@
-const nums = [12, 3, 48, 89, 31, 7, 65]
+// REDUCE
+// Code by Nico Liao
 
-const singleNums = nums.reduce((accumulator, element) => {
-    console.log(element.length)
-    if (element.length === 1) {
-        accumulator++
-    }
-    return accumulator
-}, 0)
+//No Initial Value
+const arr = [1, 2, 3, 4, 5];
+const reduceArr = arr.reduce((accumulator, currentValue) => {
+    console.log(accumulator + " " + currentValue);
+    return accumulator + currentValue
+});
 
-console.log(singleNums)
+//With Initial Value
+const arr = [1, 2, 3, 4, 5];
+const reduceArr = arr.reduce((accumulator, currentValue) => {  
+    console.log(accumulator + " " + currentValue);
+    return accumulator + currentValue
+}, 0);
+
+//Combine, Flatten Array
+const arr = [['a', 'b'], ['c', 'd'], ['e', 'f']];
+const reduceArr = arr.reduce((accumulator, currentValue) => {
+  return accumulator.concat(currentValue);
+}, []);
+console.log(reduceArr);
+
+//Counting instances of values in an object
+const arr = ['a', 'a', 'b', 'c', 'c', 'c', 'e'];
+const reduceArr = arr.reduce((accumulator, currentValue) => {
+if(accumulator[currentValue]) {
+    accumulator[currentValue] ++;
+  } else {
+    accumulator[currentValue] = 1;
+  }
+return accumulator;
+}, {});
+console.log(reduceArr);
+
+// Tasks
+// 1. Create 5 arrays and flatten them
+// 2. Create an Array of 10 people with their age and count them
