ae111a6e7b9e0cac15ae974e63ec4b75903c003a
Jeremy <jeremykung@gmail.com>
2022-04-11 13:36:10 +0800

    updated lesson for fetch api and async await

--- 13-fetch-api/script.js
+++ 13-fetch-api/script.js
@@ -1,32 +1,94 @@
-// fetch("https://api.coingecko.com/api/v3/coins/list")
-//     .then(res => console.log('resolved', res))
-//     .catch(err => console.log('rejected', err))
+// Fetch w/ Promises
 
-const getData = async (api) => {
-    const res = await fetch(api)
-    const data = await res.json()
-    return data
-}
-
-// getData("https://api.coingecko.com/api/v3/coins/list")
-//     .then(data => data)
-
-const storeData = async () => {
-    const data = await getData("https://api.coingecko.com/api/v3/coins/list")
-    console.log(data[1647])
-    document.write(data[1654].id)
-}
-
-storeData()
-
-// WORKING EXAMPLE
-
-fetch("https://api.coingecko.com/api/v3/exchanges")
-    // .then(response => response.json())
-    .then(response => console.log(response))
-    // .then(data => console.log(data))
-    .catch(err => console.log(err))
-
+    // syntax
 // fetch('https://api.coingecko.com/api/v3/exchanges')
-//   .then(response => response.json())
-//   .then(data => console.log(data));
+//     .then(response => {
+//         return response.json()
+//     })
+//     .then(jsonData => {
+//         console.log(jsonData)
+//         document.write(jsonData[0].name)
+//         // create a div in html
+//         // write the name, country, and year for one of the exchanges in the div
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     })
+
+// const promiseData = () => {
+//     fetch('https://api.coingecko.com/api/v3/exchanges')
+//         .then(response => {
+//             return response.json()
+//         })
+//         .then(data => {
+//             console.log(data)
+//         })
+//         .catch(err => {
+//             console.log(err)
+//         })
+// }
+
+// //     // shorthand
+// const promiseData = () => {
+//     fetch('https://api.coingecko.com/api/v3/exchanges')
+//         .then(response => response.json())  // auto returns
+//         .then(data => console.log(data))
+//         .catch(err => console.log(err))
+// }
+
+// recreate the fetch() function you made, and this time, store it in a variable (saved function), that you can call.
+// use shorthand if you can
+
+// Fetch w/ Async Await
+
+    // create async fn
+const getData = async () => {
+    try {
+        const response = await fetch('https://api.coingecko.com/api/v3/exchanges')
+        const data = await response.json()
+        console.log(data)
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+//     // call async fn
+// // getData()
+
+
+//     // saving data
+let myData
+
+async function fetchDataAndSave() {
+
+    const response = await fetch("https://api.coingecko.com/api/v3/exchanges")
+    
+    const jsonData = await response.json()
+    
+    myData = await jsonData
+
+}
+
+// async function fetchDataAndSave() {
+//     const response = await fetch("https://api.coingecko.com/api/v3/coins/list")
+//     const jsonData = await response.json()
+//     myData = await jsonData
+// }
+
+function promiseDataAndSave() {
+    fetch("https://api.coingecko.com/api/v3/coins/list")
+        .then(response => {
+            return response.json()
+        })
+        .then(jsonData => {
+            myData = jsonData
+        })
+}
+
+// function promiseDataAndSave() {
+//     fetch("https://api.coingecko.com/api/v3/coins/list")
+//         .then(response => response.json())
+//         .then(jsonData => myData = jsonData)
+// }
+
+// // fetchData()
